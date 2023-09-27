397ea7d8c152e3e94e011379f831cecc71d27d64
Priya Jeg <priya.sashi09@gmail.com>
2023-09-26 23:12:38 -0400

    replace hashrouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import React from "react";
 import UserTable from "./features/userDetails/userTable";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Create from "./features/userDetails/Create";
 import Edit from "./features/userDetails/Edit";
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<UserTable />} />
           <Route path="/create" element={<Create />} />
           <Route path="/edit/:id" element={<Edit />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 };
